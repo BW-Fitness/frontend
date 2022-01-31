ff025071f295eaf0302c02ed6b21475248cd1c0b
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-01-31 15:08:26 -0800

    updated styling of Navbar and InstClassList components; added axiosWithAuth; added PrivateRoute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,46 @@ import styled from "styled-components";
 
 const NavbarWrapper = styled.div`
   width: 100%;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: #47ACFF;
   color: #fff;
+  padding: 1% 0;
+`;
+
+const Title = styled.h1`
+  color: #fff;
+  margin: 0 2%;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 20%;
+`;
+
+const Button = styled.button`
+  padding: 4% 3%;
+  margin: 1%;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1.1rem;
+  width: 120px;
 `;
 
 const Navbar = () => {
   return (
     <NavbarWrapper>
-      <h1>Anywhere Fitness</h1>
-      <div>
-        <Link to='/login'>Log In</Link>
-        <Link to='/signup'>Get Started</Link>
-      </div>
+      <Title>Anywhere Fitness</Title>
+      <ButtonGroup>
+        <Link to='/login'>
+          <Button>Log In</Button>
+        </Link>
+        <Link to='/signup'>
+          <Button>Get Started</Button>
+        </Link>
+      </ButtonGroup>
     </NavbarWrapper>
   )
 }
