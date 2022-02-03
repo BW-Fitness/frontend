9c823e5975df2764f1945e0702cb37c256266d2c
Sean van der Wal <sean@vanderwal.dev>
2022-02-03 08:46:45 -0800

    Delete HomePage.js

--- src/components/HomePage.js
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-  return (
-    <div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2MYwgh60AsvEAUUqQXc-kY4gvHOqsOPtQmw&usqp=CAU" />
-    </div>
-  );
-};
