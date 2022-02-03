988be3c63c9cdbf4914af1b70159af1578f0e06c
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-03 10:13:59 -0800

    navbars updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
 
 import Navbar from './components/Navbar';
+import AuthNavbar from "./components/AuthNavbar";
 import InstClassList from './components/InstClassList';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
@@ -27,9 +28,11 @@ function App() {
             <SignupForm />
           </Route>
           <Route exact path="/instructor">
+            <AuthNavbar />
             <InstClassList />
           </Route>
           <Route exact path='/client'>
+            <AuthNavbar />
             <ClientClassList />
           </Route>
         </Switch>
